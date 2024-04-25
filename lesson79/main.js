f9c97aaaa280dbb17346535974795788cd7d428b
ychenik-A <163758092+ychenik-A@users.noreply.github.com>
2024-04-25 22:06:35 +0700

    add  lesson79

---
+++ lesson79/main.js
@@ -0,0 +1,14 @@
+const firstAge = 16
+const secondAge = 18
+
+if (firstAge >= secondAge) {
+    console.log('Вы совершеннолетний :)')
+} else  {
+    console.log('Вам ещё нет 18 :)')
+}
+
+
+// ====================================================
+
+
+firstAge === 18 ? console.log('Вы совершеннолетний :)') : console.log('Вам ещё нет 18 :)') 
