97e013e3c9ce2a567ea0768147f0ae26ab165509
ychenik-A <163758092+ychenik-A@users.noreply.github.com>
2024-05-02 22:53:35 +0700

    add lesson81

---
+++ lesson81/main.js
@@ -0,0 +1,16 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+        console.log(numbers[i]);
+    }
+}
+
+
+
+const rainbowColors = ['красный', 'оранжевый', 'желтый', 'зеленый', 'голубой', 'синий', 'фиолетовый']
+
+for (let i = rainbowColors.length - 1; i >= 0; i--) {
+    console.log(rainbowColors[i]);
+}
+
