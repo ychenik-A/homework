f859f578da0a0abb8749a761c6ccf45f78eb9167
ychenik-A <163758092+ychenik-A@users.noreply.github.com>
2024-05-08 20:10:29 +0700

    Update main.js

--- lesson83/main.js
+++ lesson83/main.js
@@ -18,9 +18,41 @@ const user = {
     }
 
 }
-// console.log(user.i)
 user.i.hello('Aleksandr')
+
 
 // =================================================================
 
 
+const users = [
+    {
+        name: 'sanya',
+        age: 21,
+        isAdmin: false
+    },
+    {
+        name: 'tanya',
+        age: 20,
+        isAdmin: true
+    },
+    {
+        name: 'lexa',
+        age: 23,
+        isAdmin: false
+    },
+    {
+        name: 'petya',
+        age: 24,
+        isAdmin: false
+    }
+]
+
+let simpleUsers = 0
+
+for (let i = 0; i < users.length; i++) {
+    if (users[i].isAdmin === false) {
+        simpleUsers++
+    }
+}
+
+console.log(simpleUsers)
